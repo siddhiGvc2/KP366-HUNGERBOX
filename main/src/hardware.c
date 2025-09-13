ac9a048172af9ceae8883f6081150fb45ee4d7ab
siddhiGvc2 <sd@gvc.in>
2025-09-13 15:41:44 +0530

    do not mqtt when coin inserted

--- main/src/hardware.c
+++ main/src/hardware.c
@@ -369,10 +369,10 @@ void gpio_read_n_act(void)
                         sprintf(payload, "*RP:%d:%d#",LastInputPin,TotalPulses); 
                          
                         send(sock, payload, strlen(payload), 0);
-                        if(MQTTRequired)
-                        {
-                            mqtt_publish_msg(payload);
-                        }
+                        // if(MQTTRequired)
+                        // {
+                        //     mqtt_publish_msg(payload);
+                        // }
                          DisplayCoinInserted();
                           vTaskDelay(5000 / portTICK_PERIOD_MS);
                           dispayQR();
